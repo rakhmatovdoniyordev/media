bb7650f2d84d0f6e4d33a7cdc0e7853dc2a576af
rakhmatovdoniyordev <rakhmatovdoniyordev@gmail.com>
2024-08-11 06:44:55 +0500

    mocom

--- js/app.js
+++ js/app.js
@@ -3,12 +3,3 @@ const collection = document.querySelector(".navbar__collection")
 function navbarToggle(){
     collection.classList.toggle("show")
 }
-
-
-var loader = document.querySelector(".loader");
-
-window.addEventListener("load", function () {
-    this.setTimeout(function() {
-        loader.style.display = "none";
-    }, 1500);
-});
